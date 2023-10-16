ee2da5808be34bd7db5236e47d2fbf24b18a263e
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-10-16 18:46:13 +0200

    [ADD] card-h2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,34 @@
 <template>
-  <Card />
+  <main>
+    <Card 
+      v-for="element in store.cardFilm" 
+      :key="element.id"
+      :film="element"
+      />
+  </main>
 </template>
 
 <script>
 import Card from './partials/Card.vue';
+import { store } from '../data/store';
 
 export default {
   name:'Main',
   components:{
     Card,
-  }
+  },
+  data(){
+    return{
+      store
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+  @use '../scss/partials/variables' as *;
 
+  main{
+    background-color: $bg-main;
+  }
 </style>
--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -1,10 +1,18 @@
 <template>
-  
+  <div>
+    <h2>{{ film.title }}</h2>
+    <h2>{{ film.original_title }}</h2>
+    <h2>{{ film.original_language }}</h2>
+    <h2>{{ film.vote_average }}</h2>
+  </div>
 </template>
 
 <script>
 export default {
-  name:'Card'
+  name:'Card',
+  props:{
+    film:Object,
+  }
 }
 </script>
 
