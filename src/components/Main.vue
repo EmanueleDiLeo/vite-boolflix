e34602797bb3f9e19eb121ac4072bdf4104f8ae7
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-10-16 13:00:15 +0200

    [INIT]

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <Card />
+</template>
+
+<script>
+import Card from "./partials/Card";
+
+export default {
+  name:'Main',
+  components:{
+    Card,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
